190ff246dcf7a00d606631021a664eff5721f7dc
jgfn1 <jgfn@cin.ufpe.br>
2018-03-16 10:54:46 -0300

    Slide 3 questions finished.

--- slide3.hs
+++ slide3.hs
@@ -24,13 +24,6 @@ tableSales n = tableSales (n - 1) ++ show (n) ++
 printTable :: Int->IO()
 printTable n = putStr ("Semana   Venda\n" ++ tableSales n)
 
-{-smallerBigger :: Int -> Int -> Int -> (Int, Int)
-smallerBigger a b c | a > b && a > c && b > c = (c, a)
-                    | otherwise a > b && a > c && c > b = (b, a)
-                    | otherwise b > a && b > c && a > c = (c, b)
-                    | otherwise b > a && b > c && c > a = (a, b)
-                    |-}
-
 smallerBigger :: Int -> Int -> Int -> (Int, Int)
 smallerBigger a b c  | (a > c && b > c) = (c, (max a b))
                      | (a > b && c > b) = (b, (max a c))
@@ -41,3 +34,20 @@ orderTriple (a, b, c) | (a < (max b c) && a > (min b c)) = ((min b c) , a, (max 
                       | (b < (max a c) && b > (min a c)) = ((min a c) , b, (max a c))
                       | (c < (max b a) && c > (min b a)) = ((min b a) , c, (max b a))
  
+type Point = (Float, Float)
+type Line = (Point, Point)
+
+fstCoordinate :: Point -> Float
+fstCoordinate (a, b) = a
+
+sndCoordinate :: Point -> Float
+sndCoordinate (a, b) = b
+
+isVertical :: Line -> Bool
+isVertical (p1, p2) = (fstCoordinate(p1) == fstCoordinate(p2))
+
+slope :: Line -> Float
+slope ((x1, y1), (x2, y2)) = (y2 - y1)/(x2 - x1)
+
+yPoint :: Float -> Line -> Float
+yPoint x line = (slope line)*x
