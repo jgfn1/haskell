f6f0420a6fdadab3cabf7ff4075ed6cc1012fd1b
jgfn1 <jgfn@cin.ufpe.br>
2018-03-16 10:17:40 -0300

    All previous exercises correctec and more exercises done.

--- slide3.hs
+++ slide3.hs
@@ -23,3 +23,21 @@ tableSales n = tableSales (n - 1) ++ show (n) ++
 
 printTable :: Int->IO()
 printTable n = putStr ("Semana   Venda\n" ++ tableSales n)
+
+{-smallerBigger :: Int -> Int -> Int -> (Int, Int)
+smallerBigger a b c | a > b && a > c && b > c = (c, a)
+                    | otherwise a > b && a > c && c > b = (b, a)
+                    | otherwise b > a && b > c && a > c = (c, b)
+                    | otherwise b > a && b > c && c > a = (a, b)
+                    |-}
+
+smallerBigger :: Int -> Int -> Int -> (Int, Int)
+smallerBigger a b c  | (a > c && b > c) = (c, (max a b))
+                     | (a > b && c > b) = (b, (max a c))
+                     | (b > a && c > a) = (a, (max b c))
+
+orderTriple :: (Int, Int, Int) -> (Int, Int, Int)
+orderTriple (a, b, c) | (a < (max b c) && a > (min b c)) = ((min b c) , a, (max b c))
+                      | (b < (max a c) && b > (min a c)) = ((min a c) , b, (max a c))
+                      | (c < (max b a) && c > (min b a)) = ((min b a) , c, (max b a))
+ 
