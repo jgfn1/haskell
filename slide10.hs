2f5e71fec02f67b30471d021f816cf0c56ee755c
jgfn1 <jgfn@cin.ufpe.br>
2018-04-17 14:45:29 -0300

    Finished slide10.hs question given from the professor.

--- slide10.hs
+++ slide10.hs
@@ -3,13 +3,14 @@ main = do name <- getLine
           file <- readFile name
           putStr file
           -- putStr uniqueList file
-          let elems = uniqueList file in putStrLn (format file (show [(countOccur file x) | x <- elems, (elem x (['a'..'z'] ++ ['0'..'9']))]))
+          let elems = uniqueList file in putStrLn (format elems (concatListOfLists ([(show (countOccur file x)) | x <- elems, (elem x (['a'..'z'] ++ ['0'..'9']))])))
 
 format :: String -> String -> String
 format [] _ = ""
 format _ [] = ""
 format (a:as) (b:bs) | (elem a (['a'..'z'] ++ ['0'..'9'])) = [a] ++ " --> " ++ [b] ++ "\n" ++ format as bs
                      | otherwise = format as (b:bs)
+
 countOccur :: String -> Char -> Int
 countOccur [] _ = 0
 countOccur list x = length [y | y <- list, y == x]
@@ -22,3 +23,7 @@ uniqueList :: String -> String
 uniqueList [] = []
 uniqueList (head:tail) | not (elem head tail) = [head]  ++ uniqueList tail
                        | otherwise = uniqueList tail
+
+concatListOfLists :: [[t]] -> [t]
+concatListOfLists [] = []
+concatListOfLists (a:as) = a ++ concatListOfLists as
